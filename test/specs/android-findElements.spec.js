0dce40470a325f659aaae530e32d59b67f50bd28
praveentestvagrant <praveenn@testvagrant.com>
2023-12-30 20:31:00 +0530

    Find element by xpath

--- test/specs/android-findElements.spec.js
+++ test/specs/android-findElements.spec.js
@@ -10,9 +10,17 @@ describe('Android Elements Tests',()=>{
 
     })
 
-    it.only("Find element by class name", async()=>{
+    it("Find element by class name", async()=>{
         const className = await $('android.widget.TextView')
         console.log(await className.getText())
         await expect(className).toHaveText("API Demos")
+    })
+
+    it("Find element by xpath", async()=>{
+        await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
+        await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
+        await $('//android.widget.TextView[@text="Command two"]').click()
+        const textAsseration = await $('//android.widget.TextView');
+        await expect(textAsseration).toHaveText("You selected: 1 , Command two");
     })
 })
