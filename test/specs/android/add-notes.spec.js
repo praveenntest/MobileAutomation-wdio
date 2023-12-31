f5a6461e9a11d0cf3bc413b513079c8eab591bee
praveentestvagrant <praveenn@testvagrant.com>
2023-12-31 21:31:56 +0530

    add note and verify note asseration

--- test/specs/android/add-notes.spec.js
+++ test/specs/android/add-notes.spec.js
@@ -3,4 +3,23 @@ describe("Add Notes",()=>{
        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip"]').click();
        await expect($('//*[@text="Add note"]')).toBeDisplayed();
     })
+
+    it('add a note, save changes & verify note', async () =>{
+        await $('//*[@text="Add note"]').click();
+        await $('//*[@text="Text"]').click();
+        await expect($('//*[@text="Editing"]')).toBeDisplayed();
+
+        //add title note
+        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_title"]').addValue("Fav Anie List");
+        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_note"]')
+        .addValue("Praveen\nis\ngood");
+
+        //save the changes
+        await driver.back();
+        await driver.back();
+
+        //asseration
+        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_btn"]')).toBeDisplayed();
+        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/view_note"]')).toHaveText("Praveen\nis\ngood");
+    })
 })
